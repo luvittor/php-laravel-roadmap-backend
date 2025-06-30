de7f37db40ef386403a2adaba144a04fac394a2b
Luciano Vettoretti <luciano.vittoretti@gmail.com>
2025-06-30 15:57:23 -0300

    Add versioning to API routes and update Postman collection URLs

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,14 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/user', [AuthController::class, 'user']);
-    Route::get('/ping-auth', [AuthController::class, 'pingAuth']);
+Route::prefix('v1')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/user', [AuthController::class, 'user']);
+        Route::get('/ping-auth', [AuthController::class, 'pingAuth']);
+    });
 });
+
