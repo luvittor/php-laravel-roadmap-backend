750583461c5ced47a0b874b5fffeea1cfa060501
luvittor <luciano.vittoretti@gmail.com>
2025-07-01 19:20:30 -0300

    Refactor tests with shared authHeaders

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,7 +5,15 @@ namespace Tests;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Tests\CreatesApplication;
 
+use App\Models\User;
+
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function authHeaders(User $user): array
+    {
+        $token = $user->createToken('api')->plainTextToken;
+        return ['Authorization' => 'Bearer ' . $token];
+    }
 }
